44d208536552ed40024c12107714e709ae051e04
Benjamin Looi <benjaminlooidt@gmail.com>
2019-07-31 22:05:26 +0800

    added firebase

---
+++ src/plugins/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from firebase
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDBp7fHHx5XpfA7UNogUr9d8VOkLc-Z9Lw",
+  authDomain: "ledo-fyp.firebaseapp.com",
+  databaseURL: "https://ledo-fyp.firebaseio.com",
+  projectId: "ledo-fyp",
+  storageBucket: "ledo-fyp.appspot.com",
+  messagingSenderId: "179153107381",
+  appId: "1:179153107381:web:aa44768cc157ed68"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+
+export { firebase, firestore };
