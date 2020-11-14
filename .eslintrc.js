05f4a7fa693a64f91c1c8bda146ff73d7c6185b6
benjaminlooi <benjaminlooidt@gmail.com>
2020-11-14 23:36:54 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    semi: 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase']
   },
   parserOptions: {
-    parser: "babel-eslint",
-  },
-};
+    parser: 'babel-eslint'
+  }
+}
